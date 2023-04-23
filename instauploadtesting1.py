69eab7cc99dc3c4560719c0d082f6d17f46c69e0
abhishekdoshi1 <42406086+abhishekdoshi1@users.noreply.github.com>
2023-04-23 20:24:11 +0530

    Update instauploadtesting1.py

--- instauploadtesting1.py
+++ instauploadtesting1.py
@@ -163,6 +163,8 @@ print("image saved")
 #Bot started
 bot = Bot()
 
+bot.logout()
+
 #Bot Logged into instagram with username and password
 bot.login(username="testpycode", password="Satara@123")
 
